eac1148c8d5fd5ac62007e2418c58808ab38f304
DanielSBLim <lastwin0725@gmail.com>
2019-05-27 16:50:20 +0900

    ing

--- app/src/main/java/com/example/study1/example/Ex02.java
+++ app/src/main/java/com/example/study1/example/Ex02.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.study1.R;
+
 public class Ex02 extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex02);
     }
 }
 
